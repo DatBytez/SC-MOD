f67c1819afe6c64771f592dd65b531e0301d31f5
DatBytez <tdykstr2@gmail.com>
2026-09-12 15:37:08 -0600

    Flashbang syncing iStun

--- data/sc_scripts/sc_crew_flashbang.lua
+++ data/sc_scripts/sc_crew_flashbang.lua
@@ -1,21 +1,23 @@
-local FLASHBANG_NAME = "FLASHBANG"
-local FLASHBANG_STUN = 10
-
 script.on_internal_event(Defines.InternalEvents.ACTIVATE_POWER, function(power, ship)
     if power.def.name ~= FLASHBANG_NAME then
         return Defines.Chain.CONTINUE
     end
 
+    local damage = power:GetPowerDamage()
+
+print("FLASHBANG DAMAGE:", damage)
+print("FLASHBANG STUN:", damage.iStun)
+
     for i = 0, ship.vCrewList:size() - 1 do
         local crew = ship.vCrewList[i]
 
         if crew ~= power.crew
-            and not crew.bDead -- Probably not necessary.
+            and not crew.bDead
             and crew.iRoomId == power.powerRoom
             and crew.iShipId == power.crew.iShipId
             and crew.bMindControlled then
 
-            crew.fStunTime = math.max(crew.fStunTime, FLASHBANG_STUN)
+            crew.fStunTime = math.max(crew.fStunTime, power:GetPowerDamage().iStun)
         end
     end
 
